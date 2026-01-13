ab8611cd06ffc8cce9d6b5f41957fa331af488b5
ErickBit <44269535+ErickBits@users.noreply.github.com>
2026-01-12 20:32:40 -0500

    fixeed

---
+++ server/routes/main_routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+const route = express.Router();
+import user_controller from '../controllers/user_controller.js';
+import { compareToken } from '../helpers/jwt.js';
+
+route.post('/register', user_controller.register);
+route.post('/login', user_controller.login);
+route.get('/profile', compareToken, user_controller.profile);
+route.put('/update', compareToken, user_controller.update);
+route.delete('/delete', compareToken, user_controller.delete);
+
+export default route;  
